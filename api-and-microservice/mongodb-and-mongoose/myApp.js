49512a5bd822759d709acf31988d6ce298060ada
이성일 <modolee@soblo.io>
2019-12-06 18:45:33 +0900

    Chain Search Query Helpers to Narrow Search Results

--- api-and-microservice/mongodb-and-mongoose/myApp.js
+++ api-and-microservice/mongodb-and-mongoose/myApp.js
@@ -333,7 +333,16 @@ var removeManyPeople = function(done) {
 var queryChain = function(done) {
   var foodToSearch = "burrito";
 
-  done(null/*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+      .sort('name').limit(2).select('-age')
+      .exec((err, data) => {
+        if(err) {
+          done(err);
+        } else {
+          done(null, data);
+        }
+      });
+
 };
 
 /** **Well Done !!**
